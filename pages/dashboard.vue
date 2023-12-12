bd43458e33dee0e1c8cff4128c2d1d7120be6e2b
yemref07 <yemref0716@hotmail.com>
2023-12-12 03:41:52 +0300

    Add initial dashboard page

---
+++ pages/dashboard.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>Dasboard page</div>
+  <button @click="logout">logout</button>
+</template>
+
+<script setup>
+import { useAuthStore } from '~/store/auth';
+import { useRouter } from '#vue-router';
+const router = useRouter()
+
+const authStore = useAuthStore()
+
+const {logUserOut} = authStore
+const logout = ()=>{
+    logUserOut()
+    router.push('/');
+}
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
