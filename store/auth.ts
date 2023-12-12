578bae5828bbaa306b4273b4db2b5bba34383100
yemref07 <yemref0716@hotmail.com>
2023-12-12 03:43:36 +0300

    Add auth pinia store for tracking and managing users login and logout process

---
+++ store/auth.ts
@@ -0,0 +1,77 @@
+import { defineStore } from 'pinia'
+
+export const useAuthStore = defineStore('signIn', () => {
+    const userInfo = ref()
+    const isAuthenticated = ref(false)
+    const isLoading = ref(false)
+
+    interface userPayload {
+        id: string;
+        password: string;
+    }
+
+    // ! Login with using default fecth 
+    const loginUser = async ({ id, password }:userPayload) => {
+        isLoading.value = true
+
+        try {
+            const response = await fetch('https://dummyjson.com/auth/login', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({
+                    username: id,
+                    password: password,
+                })
+            });
+            userInfo.value = await response.json()
+
+            if (userInfo.value) {
+                const token = useCookie('token');
+                token.value = userInfo.value?.token;
+                isAuthenticated.value = true
+            }
+        }
+
+        catch (error) {
+            console.error('Error while logging in:', error);
+        }
+
+        finally {
+            isLoading.value = false;
+        }
+
+    }
+
+    // ! login with using nuxt useFetch
+    // const loginUser = async ({ id, password }: userPayload) => {
+    //     const { data, pending }: any = await useFetch('https://dummyjson.com/auth/login', {
+    //         method: 'post',
+    //         headers: { 'Content-Type': 'application/json' },
+    //         body: {
+    //             username:id,
+    //             password:password,
+    //         },
+    //     });
+    //     isLoading.value = pending;
+    //     if(data.value){
+    //         const token = useCookie('token');
+    //         token.value = data.value?.token;
+    //         isAuthenticated.value = true;
+    //     }
+
+    // }
+
+
+    const logUserOut = () => {
+        const token = useCookie('token');
+        isAuthenticated.value = false;
+        token.value = null
+    }
+
+    return {
+        loginUser,
+        userInfo,
+        logUserOut,
+        isAuthenticated
+    }
+})
