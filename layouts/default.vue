6b6fc30594cd1bf5663e5d5c099e5339c02d7610
yunus <yemref0716@hotmail.com>
2024-01-08 17:43:37 +0300

    - FEAT Update default layout to include Tailwind dark-theme feature This commit introduces the Tailwind dark-theme feature to the default layout. The dark mode functionality is now managed by the 'dark mode store', enabling seamless toggling between light and dark modes.

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,13 +1,18 @@
 <template>
-  <div>
-    <slot />
+  <div :class="{ 'dark': theme }">
+    <div class="dark:bg-neutral-900 bg-milk">
+      <TheHeader />
+      <SideBar />
+      <slot />
+      <TheFooter />
+    </div>
   </div>
 </template>
 
 <script lang="ts" setup>
-
+import { useDarkModeStore } from "~/store/darkMode";
+const store = useDarkModeStore();
+const { theme } = storeToRefs(store);
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
