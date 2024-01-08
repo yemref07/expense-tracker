080c5752c81225bcc51711cd71f4a9f859f939cc
yunus <yemref0716@hotmail.com>
2024-01-08 17:39:34 +0300

    -Add btn-orange uı compenent

---
+++ components/buttons/btn-orange.vue
@@ -0,0 +1,12 @@
+<template>
+  <button
+    type="button"
+    class="focus:outline-none text-white bg-orange-700 hover:bg-orange-800 focus:ring-4 focus:ring-orange-300 font-medium rounded-lg text-sm px-8 py-2.5 mb-2 dark:bg-orange-600 dark:hover:bg-orange-700 dark:focus:ring-orange-900"
+  >
+    <slot></slot>
+  </button>
+</template>
+
+<script setup></script>
+
+<style lang="css" scoped></style>
