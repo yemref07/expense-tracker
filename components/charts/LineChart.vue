707e7ee8fd08850f8ecf2518762d6a666fa890e9
yunus emre fersiz <yemref0716@hotmail.com>
2024-02-02 00:54:33 +0300

    - Add Line Chart compenent its expect two props options and data

---
+++ components/charts/LineChart.vue
@@ -0,0 +1,49 @@
+<template>
+  <Line :data="data" :options="options" class="mx-auto w-full" />
+</template>
+
+<script lang="ts">
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Line } from "vue-chartjs";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+export default {
+  name: "LineChart",
+  components: { Line },
+  props: {
+    data: {
+      type:Object,
+      required:true
+    },
+    options:{
+      type:Object,
+      default: () => {
+        return {
+            responsive: true,
+        }
+    }
+  }
+
+  },
+};
+</script>
+
+<style scoped></style>
