495d07d972c5b725cc254ad074664ecba9d0d6f1
yunus emre fersiz <yemref0716@hotmail.com>
2024-02-02 00:50:51 +0300

    - Add container compenent, reusable uı element

---
+++ components/container.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="container mx-auto px-5 md:px-10 lg:px-12 xl:px-20 relative">    
+    <slot></slot>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="css" scoped>
+
+</style>
