76bb45dfb86c859e0ad02b56a5e4177b03241541
yunus emre fersiz <yemref0716@hotmail.com>
2024-02-02 00:55:59 +0300

    - Add Polar Area Chart Compenent

---
+++ components/charts/PolarAreaChart.vue
@@ -0,0 +1,41 @@
+<template>
+  <PolarArea :data="data" :options="options" />
+</template>
+
+<script lang="ts">
+import {
+  Chart as ChartJS,
+  RadialLinearScale,
+  ArcElement,
+  Tooltip,
+  Legend
+} from 'chart.js'
+import { PolarArea } from 'vue-chartjs'
+
+
+ChartJS.register(RadialLinearScale, ArcElement, Tooltip, Legend)
+
+export default {
+  name: 'PolarAreaChart',
+  components: {
+    PolarArea
+  },
+ props:{
+  options:{
+      default:()=>{
+          return{
+              plugins: {
+                legend: {
+                    display: false,
+                },
+            },
+        }
+    }
+  },
+  data:{
+      type:Object,
+      required:true
+  }
+ }
+}
+</script>
