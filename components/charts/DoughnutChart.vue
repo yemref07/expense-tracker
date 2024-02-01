d0588db928a853397c4e0f39897694f79043661b
yunus emre fersiz <yemref0716@hotmail.com>
2024-02-02 00:56:15 +0300

    Add Doughnut Chart Compenent

---
+++ components/charts/DoughnutChart.vue
@@ -0,0 +1,41 @@
+<template>
+  <Doughnut :data="chartData" :options="chartOptions" class="mx-auto w-full h-1/2 "/>
+</template>
+
+<script>
+import { Doughnut } from "vue-chartjs";
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+  CategoryScale,
+  LinearScale,
+  ArcElement,
+} from "chart.js";
+ChartJS.register(
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+  CategoryScale,
+  LinearScale,
+  ArcElement
+);
+
+export default {
+  name: "DoughnutChart",
+  components: { Doughnut },
+  props: {
+    chartData: {
+      type: Object,
+      required: true,
+    },
+    chartOptions: {
+      type: Object,
+      default: () => {}
+    }
+  },
+};
+</script>
