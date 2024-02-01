9d297ad1438c41ef2b124e6aa5e8be23d4a4f1c0
yunus emre fersiz <yemref0716@hotmail.com>
2024-02-02 01:00:21 +0300

    - Add Radar Chart compenent

---
+++ components/charts/RadarChart.vue
@@ -0,0 +1,58 @@
+<template>
+  <Radar :data="data" :options="options" />
+</template>
+
+<script lang="ts">
+import {
+  Chart as ChartJS,
+  RadialLinearScale,
+  PointElement,
+  LineElement,
+  Filler,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Radar } from "vue-chartjs";
+
+ChartJS.register(
+  RadialLinearScale,
+  PointElement,
+  LineElement,
+  Filler,
+  Tooltip,
+  Legend
+);
+
+export default {
+  name: "RadarChart",
+  components: {
+    Radar,
+  },
+  props: {
+    options: {
+      type: Object,
+      default: () => {
+        return {
+          responsive: true,
+          scales: {
+            y: {
+              // defining min and max so hiding the dataset does not change scale range
+              min: 0,
+              max: 3000,
+            },
+          },
+          plugins: {
+            legend: {
+              display: false,
+            },
+          },
+        };
+      },
+    },
+    data: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
