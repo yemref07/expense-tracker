616cdd9765dc979927784d30db4177e61ba3f392
yunus emre fersiz <yemref0716@hotmail.com>
2024-02-02 00:55:43 +0300

    - Add Bar hart compenent its expect two props options and data , also it has some defualt options too

---
+++ components/charts/BarChart.vue
@@ -0,0 +1,49 @@
+<template>
+  <Bar
+    id="my-chart-id"
+    :options="options"
+    :data="data"
+    class="mx-auto w-full p-2"
+  />
+</template>
+
+<script>
+import { Bar } from "vue-chartjs";
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+  CategoryScale,
+  LinearScale,
+} from "chart.js";
+
+ChartJS.register(
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+  CategoryScale,
+  LinearScale
+);
+
+export default {
+  name: "BarChart",
+  components: { Bar },
+  props: {
+    data: {
+      type: Object,
+      required: true,
+    },
+    options: {
+      type: Object,
+      default: () => {
+        return { responsive: true };
+      },
+    },
+  },
+};
+</script>
+
+<style scoped></style>
